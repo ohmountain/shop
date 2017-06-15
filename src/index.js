afba273999ff65adcb5709d770997e2f5dd55925
renshan <1005110700@qq.com>
2017-06-15 12:02:35 +0800

    add normalize.cs

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import './styles/normalize.css';
 import './styles/menu.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
