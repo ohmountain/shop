1abb4747856db262c516c88bc464d1fae218da36
renshan <1005110700@qq.com>
2017-06-20 11:34:45 +0800

    add font-family to body

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './styles/normalize.css';
+import './styles/common.css';
 import './styles/menu.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
